d2cb2739fa540dd9aa9356e3f2f0d3d8bb66f749
ZARUJU <0613akb48@gmail.com>
2023-06-01 13:01:08 +0900

    Update 2file

--- py-AAA/split.py
+++ py-AAA/split.py
@@ -1,33 +1,32 @@
 import re
 import datetime
 import os
+import shutil
 
 delimiter = "起床"
-start_date = datetime.date(2023, 4, 17)  # ファイル名の最初の日付
+start_date = datetime.date(2023, 5, 1)  # ファイル名の最初の日付
 output_directory = "./split-output"  # 出力先ディレクトリ
 
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+# 既存の split-output フォルダが存在する場合は削除
+if os.path.exists(output_directory):
+    shutil.rmtree(output_directory)
+
+# split-output フォルダを作成
+os.makedirs(output_directory)
+
+# .gitkeep ファイルを生成
+gitkeep_file = os.path.join(output_directory, ".gitkeep")
+with open(gitkeep_file, 'w'):
+    pass
 
 with open('result.md', 'r') as input_file:
-    lines = input_file.readlines()
+    content = input_file.read()
 
-segments = []
-current_segment = ""
-
-for line in lines:
-    if delimiter in line:
-        if current_segment:
-            segments.append(current_segment)
-            current_segment = ""
-    current_segment += line
-
-if current_segment:
-    segments.append(current_segment)
+segments = re.split(delimiter, content)
 
 for i, segment in enumerate(segments):
     current_date = start_date + datetime.timedelta(days=i)
     formatted_date = current_date.strftime("%Y%m%d")
     output_file_name = os.path.join(output_directory, f"{formatted_date}.md")
     with open(output_file_name, 'w') as output_file:
-        output_file.write(segment)
+        output_file.write(formatted_date + "\n" + segment)
